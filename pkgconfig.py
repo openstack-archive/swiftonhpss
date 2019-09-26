c6b4099c2a2be10523ee8d31cac03ca642111458
Jim Rollenhagen <jim@jimrollenhagen.com>
2019-09-26 09:50:21 -0400

    Retire github mirror, repo moved to opendev

--- pkgconfig.py
+++
@@ -1,8 +0,0 @@
-# Simple program to save all package information
-# into a file which can be sourced by a bash script
-
-from swiftonhpss.swift import _pkginfo as pkginfo
-
-PKGCONFIG = 'pkgconfig.in'
-
-pkginfo.save_config(PKGCONFIG)
